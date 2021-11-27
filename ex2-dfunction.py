200c2ccbcce64a6bfcdb569532d5f986aa9dc7a0
Injamul Hoque <injamulhoque005@gmail.com>
2021-11-28 04:01:56 +0600

    2nd commit: modified ex1 and ex2 python files

---
+++ ex2-dfunction.py
@@ -0,0 +1,8 @@
+def double(n):
+    return 2 * n        #Return twice the given number
+#Call the function with the value of your choice and print its result
+x = double(3)
+print(x)
+print("The program ran successfully")
+#..................................................
+print("First modification for 2nd commit")
